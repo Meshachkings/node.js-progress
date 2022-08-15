c07cdc312a75af68a7ea26a2c34b5da4724715b2
meshachkings <meshachkings@gmail.com>
2022-08-15 18:24:30 +0100

    master

--- nodejs blog/app.js
+++ nodejs blog/app.js
@@ -1,4 +1,5 @@
-const express = require ('express');
+const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -8,6 +9,18 @@ app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(3000);
+
+app.use(express.static('public'));
+
+//-------------------- mmiddleware [note: next()] ------------------------
+// app.use((req, res, next) => {
+//     console.log('new request made: ');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+//     next();
+// });
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     // blogs
