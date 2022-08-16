a1580877c4795b848a48f4b3ac0c05ba5348cd66
meshachkings <meshachkings@gmail.com>
2022-08-16 15:50:49 +0100

    master

--- nodejs blog/app.js
+++ nodejs blog/app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://meshach:meshach123@cluster0.wnthsxd.mongodb.net/cluster0?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000)) // listen for request
+    .catch((err) => console.log(err));
+
 // view engine
 app.set('view engine', 'ejs'); 
 
-// listen for request
-app.listen(3000);
-
-app.use(express.static('public'));
-
-//-------------------- mmiddleware [note: next()] ------------------------
+//-------------------- mmiddleware [note: next()] & static ------------------------
 // app.use((req, res, next) => {
 //     console.log('new request made: ');
 //     console.log('host: ', req.hostname);
@@ -20,6 +23,7 @@ app.use(express.static('public'));
 //     console.log('method: ', req.method);
 //     next();
 // });
+app.use(express.static('public'));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
